40739fa0b9291dee1c968e3b5efa0939b7b67497
Kegits <kelvingithaithi@gmail.com>
2023-11-15 09:18:02 +0300

    Made some changes

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -26,7 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home, name="Home"),
     path('blog/', include('blog.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
